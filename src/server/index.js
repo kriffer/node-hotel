2840ab1cde8c8c53ce366e98c145645e6e44660e
kriffer <antonio.kravets@gmail.com>
2018-11-14 01:31:22 +0200

    refactored server side

--- src/server/index.js
+++ src/server/index.js
@@ -10,10 +10,8 @@ app.listen(8080, () => console.log("Listening on port 8080!"));
 
 app.get("/", (req, res, next) => {});
 
-// Handles any requests that don't match the ones above
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
+// 
+
 
 // app.get('/', (req, res) => {
 //     res.send('');

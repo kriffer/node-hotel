f0e76f1f16a8640f5d5c0fe3995910e372ae2da4
kriffer <antonio.kravets@gmail.com>
2018-11-14 00:40:28 +0200

    refactor

---
+++ src/client/components/Article.js
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+import React from 'react';
+import { Segment, Container, Header, Button, Divider } from "semantic-ui-react";
+
+class Article extends Component {
+    constructor(props) {
+        super(props);
+      }
+
+  render() {
+    return (
+      <Segment style={{ padding: "8em 0em" }} vertical>
+        <Container text>
+          <Header as="h3" style={{ fontSize: "2em" }}>
+            Breaking The Grid, Grabs Your Attention
+          </Header>
+          <p style={{ fontSize: "1.33em" }}>
+            Instead of focusing on content creation and hard work, we have
+            learned how to master the art of doing nothing by providing massive
+            amounts of whitespace and generic content that can seem massive,
+            monolithic and worth your attention.
+          </p>
+          <Button as="a" size="large">
+            Read More
+          </Button>
+          <Divider
+            as="h4"
+            className="header"
+            horizontal
+            style={{ margin: "3em 0em", textTransform: "uppercase" }}
+          >
+            {/* <a href="#">Case Studies</a> */}
+          </Divider>
+        </Container>
+      </Segment>
+    );
+  }
+}
+
+export default Article;
+

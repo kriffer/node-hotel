63a3007132dc95b48b1dedd2878382aa8e028be3
kriffer <antonio.kravets@gmail.com>
2018-11-14 02:23:54 +0200

    changing server port to 5000

--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,11 @@
 const express = require("express");
-
-// requre('NewsController');
+ 
 
 const app = express();
 
 app.use(express.static("dist"));
 
-app.listen(8080, () => console.log("Listening on port 8080!"));
+app.listen(process.env.PORT || 5000);
 
 app.get("/", (req, res, next) => {});
-
-// 
-
-
-// app.get('/', (req, res) => {
-//     res.send('');
-// });
+ 
